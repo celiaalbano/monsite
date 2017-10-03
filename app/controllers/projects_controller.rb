2d9695f547f2c7609b9d44e6d52af5ca05d6332c
celia albano <albano.celia@gmail.com>
2017-10-03 10:10:06 +0200

    controllers créés

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,19 @@
+class ProjectsController < ApplicationController
+  def soretec
+  end
+
+  def dmum
+  end
+
+  def ose
+  end
+
+  def archil
+  end
+
+  def fermepc
+  end
+
+  def pelemele
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+resources :projects do
+    collection do
+      get 'soretec', to: "projects#soretec"
+      get 'archil', to: "projects#archil"
+      get 'fermepc', to: "projects#fermepc"
+      get 'ose', to: "projects#ose"
+      get 'dmum', to: "projects#dmum"
+      get 'pelemele', to: "projects#pelemele"
+    end
+  end
+
+root to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
